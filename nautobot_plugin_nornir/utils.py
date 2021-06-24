89a9c4f6b33074e65674a25e84aa7cf55f4bfc9b
itdependsnetworks <ken@celenza.org>
2021-06-23 22:40:57 -0400

    run black

--- nautobot_plugin_nornir/utils.py
+++ nautobot_plugin_nornir/utils.py
@@ -3,6 +3,7 @@
 from nornir_nautobot.plugins.tasks.dispatcher import _DEFAULT_DRIVERS_MAPPING
 from nautobot_plugin_nornir.constants import PLUGIN_CFG
 
+
 def get_dispatcher():
     """Helper method to load the dispatcher from nautobot nornir or config if defined."""
     if PLUGIN_CFG.get("dispatcher_mapping"):
