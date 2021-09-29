60c984a9039c06bca9937f93e6934b05b5126458
Mikhail Yohman <mikhail.yohman@gmail.com>
2021-09-29 15:10:32 -0600

    Return empty dict instead of None when getting plugin settings in constants.py

--- nautobot_plugin_nornir/constants.py
+++ nautobot_plugin_nornir/constants.py
@@ -8,7 +8,7 @@ _NORNIR_SETTINGS = {
     "nornir.core": {"num_workers": 20},
 }
 
-PLUGIN_CFG = settings.PLUGINS_CONFIG["nautobot_plugin_nornir"]
+PLUGIN_CFG = settings.PLUGINS_CONFIG.get("nautobot_plugin_nornir", {})
 NORNIR_SETTINGS = PLUGIN_CFG.get("nornir_settings", _NORNIR_SETTINGS)
 
 DEFAULT_DRIVERS_MAPPING = PLUGIN_CFG.get("dispatcher_mapping", _DEFAULT_DRIVERS_MAPPING)
