2c5c8eddc17104401ef9f44f0f037b62e965056f
Jeff Kala <jeff.kala@networktocode.com>
2021-12-29 11:58:34 -0600

    add extra for connections

--- development/nautobot_config.py
+++ development/nautobot_config.py
@@ -7,7 +7,6 @@ import sys
 from nautobot.core.settings import *  # noqa: F403
 from nautobot.core.settings_funcs import parse_redis_connection
 
-
 #
 # Misc. settings
 #
@@ -132,6 +131,12 @@ PLUGINS = ["nautobot_plugin_nornir"]
 # Each key in the dictionary is the name of an installed plugin and its value is a dictionary of settings.
 PLUGINS_CONFIG = {
     "nautobot_plugin_nornir": {
+        "napalm_extras": {"global_delay_factor": 5},
+        "netmiko_extras": {
+            "global_delay_factor": 5,
+            "banner_timeout": 40,
+            "conn_timeout": 30,
+        },
         "nornir_settings": {
             "credentials": "nautobot_plugin_nornir.plugins.credentials.env_vars.CredentialsEnvVars",
             "runner": {
