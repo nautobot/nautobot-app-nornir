525238ec7683c4102a667562e3fdc6fef4d0f1af
Jeff Kala <jeff.kala@networktocode.com>
2022-01-21 16:54:58 -0600

    fix dict key call

--- nautobot_plugin_nornir/plugins/inventory/nautobot_orm.py
+++ nautobot_plugin_nornir/plugins/inventory/nautobot_orm.py
@@ -181,7 +181,7 @@ class NautobotORMInventory:
                 if nornir_options.get("connection_secret_path"):
                     secret_path = nornir_options.pop("connection_secret_path")
                 elif CONNECTION_SECRETS_PATHS.get(nornir_provider):
-                    secret_path = CONNECTION_SECRETS_PATHS["nornir_provider"]
+                    secret_path = CONNECTION_SECRETS_PATHS[nornir_provider]
                 else:
                     continue
                 _set_dict_key_path(conn_options, secret_path, secret)
