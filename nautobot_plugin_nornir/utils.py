94f175ac65479ca6530f7e383a4e6ce34956f4cf
itdependsnetworks <ken@celenza.org>
2022-09-27 07:26:30 -0400

    Merge dispatcher rather than overwrite

--- nautobot_plugin_nornir/utils.py
+++ nautobot_plugin_nornir/utils.py
@@ -7,5 +7,5 @@ from nautobot_plugin_nornir.constants import PLUGIN_CFG
 def get_dispatcher():
     """Helper method to load the dispatcher from nautobot nornir or config if defined."""
     if PLUGIN_CFG.get("dispatcher_mapping"):
-        return PLUGIN_CFG["dispatcher_mapping"]
+        return {**_DEFAULT_DRIVERS_MAPPING, **PLUGIN_CFG["dispatcher_mapping"]}
     return _DEFAULT_DRIVERS_MAPPING
