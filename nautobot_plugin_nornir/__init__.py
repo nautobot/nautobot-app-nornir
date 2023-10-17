9a64a8cb787519daf2715865acee3f96c262f4bc
bakebot <info@networktocode.com>
2023-10-17 14:01:16 +0000

    Cookie initialy baked by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "develop",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-plugin-nornir.git",
        "path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-nornir",
        "repository_path": "/opt/ntc/drift-manager/outputs/nautobot-plugin-nornir",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "@smith-ntc",
            "full_name": "Network to Code, LLC",
            "email": "info@networktocode.com",
            "github_org": "nautobot",
            "plugin_name": "nautobot_plugin_nornir",
            "verbose_name": "Nautobot Nornir Plugin",
            "plugin_slug": "nautobot-plugin-nornir",
            "project_slug": "nautobot-plugin-nornir",
            "repo_url": "https://github.com/nautobot/nautobot-plugin-nornir",
            "base_url": "plugin-nornir",
            "min_nautobot_version": "2.0.0",
            "max_nautobot_version": "2.9999",
            "camel_name": "NautobotPluginNornir",
            "project_short_description": "Nautobot Nornir Plugin",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/plugin-nornir/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/opt/ntc/drift-manager/outputs",
            "_repo_dir": "/opt/ntc/drift-manager/outputs/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "develop"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.UPDATE_OR_CREATE",
        "post_actions": [
            "PostAction.BLACK"
        ],
        "baked_commit_ref": "",
        "draft": true
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "setup-cookie-nornir.json",
        "output_dir": "./outputs",
        "push": true,
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "template_dir": "nautobot-app",
        "template_ref": "develop",
        "pull_request": "update-or-create",
        "post_action": [
            "black"
        ],
        "disable_post_actions": false,
        "draft": true
    }
    ```

--- nautobot_plugin_nornir/__init__.py
+++ nautobot_plugin_nornir/__init__.py
@@ -1,24 +1,20 @@
 """Plugin declaration for nautobot_plugin_nornir."""
-
+# Metadata is inherited from Nautobot. If not including Nautobot in the environment, this should be added
 from importlib import metadata
-
 
 __version__ = metadata.version(__name__)
 
-from nautobot.extras.plugins import PluginConfig
-from nautobot_plugin_nornir.utils import verify_setting
+from nautobot.extras.plugins import NautobotAppConfig
 
 
-class NornirConfig(PluginConfig):
-    """Plugin configuration for nautobot_plugin_nornir."""
+class NautobotPluginNornirConfig(NautobotAppConfig):
+    """Plugin configuration for the nautobot_plugin_nornir plugin."""
 
     name = "nautobot_plugin_nornir"
-    verbose_name = "Nautobot Plugin for Nornir"
+    verbose_name = "Nautobot Nornir Plugin"
     version = __version__
     author = "Network to Code, LLC"
-    description = (
-        "Nautobot App that provides a shim layer to simplify using Nornir within other Nautobot Apps and Nautobot Jobs"
-    )
+    description = "Nautobot Nornir Plugin."
     base_url = "plugin-nornir"
     required_settings = []
     min_version = "2.0.0"
@@ -26,47 +22,5 @@ class NornirConfig(PluginConfig):
     default_settings = {}
     caching_config = {}
 
-    def ready(self):
-        """Check for accidental legacy configurations."""
-        from django.conf import settings  # pylint: disable=import-outside-toplevel
 
-        plugin_settings = settings.PLUGINS_CONFIG["nautobot_plugin_nornir"]
-
-        dispatcher_mapping_message = (
-            "The `dispatcher_mapping` key is no longer functional, failing early "
-            "to ensure your application works as expected, please see: "
-            "https://docs.nautobot.com/projects/plugin-nornir/en/latest/admin/migrating_to_v2/#dispatcher-settings for more details."
-        )
-        verify_setting(plugin_settings, "dispatcher_mapping", dispatcher_mapping_message)
-        custom_dispatcher_message = (
-            "The `custom_dispatcher` key is only meant for `golden_config_plugin` settings "
-            "and not `nautobot_plugin_nornir` settings, please see: "
-            "https://docs.nautobot.com/projects/plugin-nornir/en/latest/admin/migrating_to_v2/#dispatcher-settings for more details."
-        )
-        verify_setting(plugin_settings, "custom_dispatcher", custom_dispatcher_message)
-        verify_setting(
-            plugin_settings, "credentials", "The `credentials` key should be within the `nornir_settings` dictionary"
-        )
-        verify_setting(
-            plugin_settings, "runners", "The `runners` key should be within the `nornir_settings` dictionary"
-        )
-        verify_setting(
-            plugin_settings,
-            "nornir_settings.connection_options",
-            "The `connection_options` key should NOT be within the `nornir_settings` dictionary",
-        )
-        verify_setting(
-            plugin_settings,
-            "nornir_settings.use_config_context",
-            "The `use_config_context` key should NOT be within the `nornir_settings` dictionary",
-        )
-        # TODO: Commenting the below out, which I believe are correct but want to verify first
-        # verify_setting(plugin_settings, "nornir_settings.username", "The `username` key should NOT be within the `nornir_settings` dictionary")
-        # verify_setting(plugin_settings, "nornir_settings.password", "The `password` key should NOT be within the `nornir_settings` dictionary")
-        # verify_setting(plugin_settings, "nornir_settings.secret", "The `secret` key should NOT be within the `nornir_settings` dictionary")
-        # verify_setting(plugin_settings, "nornir_settings.connection_secret_path", "The `connection_secret_path` key should NOT be within the `nornir_settings` dictionary")
-        # verify_setting(plugin_settings, "nornir_settings.secret_access_type", "The `secret_access_type` key should NOT be within the `nornir_settings` dictionary")
-        super().ready()
-
-
-config = NornirConfig  # pylint:disable=invalid-name
+config = NautobotPluginNornirConfig  # pylint:disable=invalid-name
