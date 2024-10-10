10810b960b3fd351f64068169653793051d9e53e
bakebot <info@networktocode.com>
2024-10-10 18:53:19 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "refs/tags/nautobot-app-v2.4.0",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-app-nornir.git",
        "path": "/tmp/tmplo93pg6j/nautobot-app-nornir",
        "repository_path": "/tmp/tmplo93pg6j/nautobot-app-nornir",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "-* @itdependsnetworks\n-* @jeffkala",
            "full_name": "Network to Code, LLC",
            "email": "info@networktocode.com",
            "github_org": "nautobot",
            "app_name": "nautobot_plugin_nornir",
            "verbose_name": "Nautobot Nornir Plugin",
            "app_slug": "nautobot-plugin-nornir",
            "project_slug": "nautobot-app-nornir",
            "repo_url": "https://github.com/nautobot/nautobot-app-nornir",
            "base_url": "plugin-nornir",
            "min_nautobot_version": "2.0.0",
            "max_nautobot_version": "2.9999",
            "camel_name": "NautobotPluginNornir",
            "project_short_description": "Nautobot App that provides a shim layer to simplify using Nornir within other Nautobot Apps and Nautobot Jobs",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/plugin-nornir/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/tmp/tmplo93pg6j",
            "_repo_dir": "/github/home/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "refs/tags/nautobot-app-v2.4.0"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.RUFF",
            "PostAction.POETRY"
        ],
        "baked_commit_ref": "c39f9ec50c2ea901793fdb090a7d74569b7bfdab",
        "draft": false
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "",
        "push": true,
        "template": "",
        "template_dir": "",
        "template_ref": "refs/tags/nautobot-app-v2.4.0",
        "pull_request": null,
        "post_action": [
            "ruff",
            "poetry"
        ],
        "disable_post_actions": true,
        "draft": false
    }
    ```

--- nautobot_plugin_nornir/__init__.py
+++ nautobot_plugin_nornir/__init__.py
@@ -22,6 +22,7 @@ class NautobotPluginNornirConfig(NautobotAppConfig):
     max_version = "2.9999"
     default_settings = {}
     caching_config = {}
+    docs_view_name = "plugins:nautobot_plugin_nornir:docs"
 
 
 config = NautobotPluginNornirConfig  # pylint:disable=invalid-name
