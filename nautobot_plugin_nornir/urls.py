bad9dcf39c5a3addcc21997e4218fc6504d6e1fc
bakebot <info@networktocode.com>
2025-01-07 18:41:38 +0000

    Cookie updated by NetworkToCode Cookie Drift Manager Tool
    Template:
    
    ```
    {
        "template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
        "dir": "nautobot-app",
        "ref": "refs/tags/nautobot-app-v2.4.1",
        "path": null
    }
    ```
    
    Cookie:
    
    ```
    {
        "remote": "https://github.com/nautobot/nautobot-app-nornir.git",
        "path": "/tmp/tmpyz49mb0b/nautobot-app-nornir",
        "repository_path": "/tmp/tmpyz49mb0b/nautobot-app-nornir",
        "dir": "",
        "branch_prefix": "drift-manager",
        "context": {
            "codeowner_github_usernames": "-* @itdependsnetworks\n-* @jeffkala",
            "full_name": "Network to Code, LLC",
            "email": "info@networktocode.com",
            "github_org": "nautobot",
            "app_name": "nautobot_plugin_nornir",
            "verbose_name": "Nautobot Nornir Plugin",
            "app_slug": "nautobot-plugin-nornir",
            "project_slug": "nautobot-app-nornir",
            "repo_url": "https://github.com/nautobot/nautobot-app-nornir",
            "base_url": "plugin-nornir",
            "min_nautobot_version": "2.0.0",
            "max_nautobot_version": "2.9999",
            "camel_name": "NautobotPluginNornir",
            "project_short_description": "Nautobot App that provides a shim layer to simplify using Nornir within other Nautobot Apps and Nautobot Jobs",
            "model_class_name": "None",
            "open_source_license": "Apache-2.0",
            "docs_base_url": "https://docs.nautobot.com",
            "docs_app_url": "https://docs.nautobot.com/projects/plugin-nornir/en/latest",
            "_template": "https://github.com/nautobot/cookiecutter-nautobot-app.git",
            "_output_dir": "/tmp/tmpyz49mb0b",
            "_repo_dir": "/github/home/.cookiecutters/cookiecutter-nautobot-app/nautobot-app",
            "_checkout": "refs/tags/nautobot-app-v2.4.1"
        },
        "base_branch": "develop",
        "remote_name": "origin",
        "pull_request_strategy": "PullRequestStrategy.CREATE",
        "post_actions": [
            "PostAction.RUFF",
            "PostAction.POETRY"
        ],
        "baked_commit_ref": "10810b960b3fd351f64068169653793051d9e53e",
        "draft": false
    }
    ```
    
    CLI Arguments:
    
    ```
    {
        "cookie_dir": "",
        "input": false,
        "json_filename": "",
        "output_dir": "",
        "push": true,
        "template": "",
        "template_dir": "",
        "template_ref": "refs/tags/nautobot-app-v2.4.1",
        "pull_request": null,
        "post_action": [
            "ruff",
            "poetry"
        ],
        "disable_post_actions": true,
        "draft": false
    }
    ```

---
+++ nautobot_plugin_nornir/urls.py
@@ -0,0 +1,23 @@
+"""Django urlpatterns declaration for nautobot_plugin_nornir app."""
+
+from django.templatetags.static import static
+from django.urls import path
+from django.views.generic import RedirectView
+from nautobot.apps.urls import NautobotUIViewSetRouter
+
+
+# Uncomment the following line if you have views to import
+# from nautobot_plugin_nornir import views
+
+
+router = NautobotUIViewSetRouter()
+
+# Here is an example of how to register a viewset, you will want to replace views.NautobotPluginNornirUIViewSet with your viewset
+# router.register("nautobot_plugin_nornir", views.NautobotPluginNornirUIViewSet)
+
+
+urlpatterns = [
+    path("docs/", RedirectView.as_view(url=static("nautobot_plugin_nornir/docs/index.html")), name="docs"),
+]
+
+urlpatterns += router.urls
