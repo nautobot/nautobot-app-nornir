ad3840e17482232f19b879c349adb1654be0356c
Ken Celenza <ken@celenza.org>
2025-03-07 18:03:41 -0500

    Build out process for and create actual error docs (#200)
    * Build out process for and create actual error docs and Improved handling of errors when the using Secrets with config_context.
    
    
    Co-authored-by: Jeff Kala <48843785+jeffkala@users.noreply.github.com>
    ---------
    Co-authored-by: Jeff Kala <48843785+jeffkala@users.noreply.github.com>

--- nautobot_plugin_nornir/utils.py
+++ nautobot_plugin_nornir/utils.py
@@ -1,5 +1,9 @@
 """Utility function for the plugin."""
 
+from typing import Any
+
+from nautobot_plugin_nornir.error_codes import ERROR_CODES
+
 
 def verify_setting(obj, dotted_dictionary, message):
     """Helper function to ensure known problematic settings never happen."""
@@ -13,3 +17,22 @@ def verify_setting(obj, dotted_dictionary, message):
             return
 
     raise ValueError(message)
+
+
+def get_error_message(error_code: str, **kwargs: Any) -> str:
+    """Get the error message for a given error code.
+
+    Args:
+        error_code (str): The error code.
+        **kwargs: Any additional context data to be interpolated in the error message.
+
+    Returns:
+        str: The constructed error message.
+    """
+    try:
+        error_message = ERROR_CODES.get(error_code, ERROR_CODES["E2XXX"]).error_message.format(**kwargs)
+    except KeyError as missing_kwarg:
+        error_message = f"Error Code was found, but failed to format, message expected kwarg `{missing_kwarg}`."
+    except Exception:  # pylint: disable=broad-except
+        error_message = "Error Code was found, but failed to format message, unknown cause."
+    return f"{error_code}: {error_message}"
