d19e533fa6b28a26fb3386afcb367b728837068c
Jeff Kala <jeff.l.kala@gmail.com>
2025-04-01 17:42:05 -0500

    another working example

--- nautobot_plugin_nornir/jobs.py
+++ nautobot_plugin_nornir/jobs.py
@@ -1,7 +1,84 @@
+import json
+
+from nautobot.apps.jobs import BooleanVar, Job, ObjectVar, register_jobs
+from nautobot.dcim.models import Device
+from nornir import InitNornir
+from nornir.core.plugins.inventory import InventoryPluginRegister
+from nornir.core.plugins.runners import RunnersPluginRegister
+from nornir.core.task import Result, Task
+
+from nautobot_plugin_nornir.plugins.inventory.nautobot_orm import NautobotORMInventory
+from nautobot_plugin_nornir.plugins.runner.celery_runner import CeleryTaskRunner
+
+InventoryPluginRegister.register("nautobot-inventory", NautobotORMInventory)
+RunnersPluginRegister.register("celery-runner", CeleryTaskRunner)
+
 from nautobot.core.celery import nautobot_task
-from nornir.core.task import Result
+
+# lassnornir.core.task.Task(task: Callable[[...], Any], nornir: Nornir, global_dry_run: bool, processors: Processors, name: str | None = None, severity_level: int = 20, parent_task: Task | None = None, **kwargs: str)
+
+
+# @nautobot_task(soft_time_limit=1800, time_limit=2000)
+# def run_nornir_packaged_task(nornir_task, hosts) -> dict:
+#     print(f"Running task: {nornir_task}")
+#     print(f"Hosts: {hosts}")
+#     task_func = import_string("nautobot_plugin_nornir.jobs.hello_world")
+#     print(f"Task Func: {type(task_func)}")
+#     # Task(task_func)
+#     result = {}
+#     for host in hosts:
+#         mr = task_func.copy().start(host)
+#         result[host.name] = mr
+#     return result
 
 
 @nautobot_task(soft_time_limit=1800, time_limit=2000)
-def hello_world(host) -> Result:
-    return Result(host="hostname", result="says hello world!", failed=False)
+def hello_world(task: Task, host) -> Result:
+    print(f"In hello_world task: {host}")
+    result = Result(host=host, result=f"{task} says hello world!", failed=False)
+    meh = vars(result)
+    meh["host"]["obj"] = meh["host"]["obj"].name
+    print(json.dumps(meh))
+    return json.dumps(meh)
+
+
+# {'result': 'hello_world says hello world!', 'host': {'id': '5f4122f3-0f64-4d96-abd7-bad15ab1a1fa', 'type': 'CSR1000v', 'location': 'site-1_09ea', 'role': 'router', 'config_context': {}, 'custom_field_data': {}, 'obj': <SimpleLazyObject: <Device: csr-0>>, 'ansible_driver': 'cisco.ios.ios', 'hier_config_driver': 'ios', 'napalm_driver': 'ios', 'netmiko_driver': 'cisco_ios', 'netutils_parser_driver': 'cisco_ios', 'ntc_templates_driver': 'cisco_ios', 'pyats_driver': 'iosxe', 'pyntc_driver': 'cisco_ios_ssh', 'scrapli_driver': 'cisco_iosxe', 'connection_options': {'napalm': {'extras': {'optional_args': {'global_delay_factor': 1, 'secret': 'cisco'}}, 'platform': 'ios'}, 'netmiko': {'extras': {'global_delay_factor': 1, 'secret': 'cisco'}, 'platform': 'cisco_ios'}, 'scrapli': {'platform': 'cisco_iosxe'}, 'pyntc': {'platform': 'cisco_ios_ssh'}}, 'now': 'now'}, 'changed': False, 'diff': '', 'failed': False, 'exception': None, 'name': None, 'severity_level': 20, 'stdout': None, 'stderr': None}
+
+
+class NornirFunTask(Job):  # pylint: disable=too-many-instance-attributes
+    """Nautobot Job for checking a tcp port is 'opened'."""
+
+    device = ObjectVar(model=Device, required=True)
+    debug = BooleanVar(description="Enable for more verbose debug logging")
+
+    class Meta:
+        """Meta object for Nornir Learning."""
+
+        name = "Execute Nornir in Celery Runner - Single Device"
+        description = "Celery Runner Fun."
+        has_sensitive_variables = False
+
+    def run(self, *args, **kwargs):
+        """Process tcp_ping task from job."""
+        self.logger.info(f"{kwargs['device']}")
+        qs = Device.objects.filter(id=kwargs["device"].id)
+        nr = InitNornir(
+            runner={
+                "plugin": "celery-runner",
+            },
+            logging={"enabled": False},
+            inventory={
+                "plugin": "nautobot-inventory",
+                "options": {
+                    "credentials_class": "nautobot_plugin_nornir.plugins.credentials.env_vars.CredentialsEnvVars",
+                    "queryset": qs,
+                    "defaults": {"now": "now"},
+                },
+            },
+        )
+        # print(nr.inventory.hosts)
+        agg_result = nr.run(task=hello_world)
+        # return agg_result
+
+
+register_jobs(NornirFunTask)
--- nautobot_plugin_nornir/plugins/runner/celery_runner.py
+++ nautobot_plugin_nornir/plugins/runner/celery_runner.py
@@ -1,9 +1,10 @@
-from typing import List
-
-from celery import group, signature
+from celery import signature
+from celery.result import AsyncResult  # Import AsyncResult
 from celery.utils.nodenames import gethostname
-from nornir.core.inventory import Host
-from nornir.core.task import AggregatedResult, Task
+from nautobot.core.celery import app
+
+# from celery_tasks import app  # import the celery app
+from nornir.core.task import AggregatedResult, MultiResult, Result
 
 
 class CeleryTaskRunner:
@@ -12,27 +13,110 @@ class CeleryTaskRunner:
     def __init__(self) -> None:
         pass
 
-    def run(self, task: Task, hosts: List[Host]) -> AggregatedResult:
-        print("Running task in Celery")
-        print(f"Task: {task}")
-        print(f"Hosts: {hosts}")
-        # print(dir(hosts[0]))
-        result = AggregatedResult(task.name)
-        group_of_tasks = [
-            signature(
+    def run(self, task, hosts, **kwargs):
+        multi_result = MultiResult(task.name)
+        async_results = {}
+
+        # Dispatch Celery tasks
+        for host in hosts:
+            async_result = signature(
                 "nautobot_plugin_nornir.jobs.hello_world",
-                args=(dict(host),),
+                args=(task.name, dict(host)),
                 kwargs={},
                 routing_key="default",
                 hostname=gethostname(),
-            )
-            for host in hosts
-        ]
-        g = group(group_of_tasks)
-        res = g()
-        # result[host.name] = task.copy().start(host)
-        for gr in res.results:
-            print(gr.status)
-        for host in hosts:
-            result[host.name] = gr.result
-        return result
+            ).delay()
+            async_results[host.name] = async_result.id  # store the task ID.
+
+        # Retrieve results from the Celery backend
+        for hostname, task_id in async_results.items():
+            async_result = AsyncResult(task_id, app=app)  # create a new AsyncResult object from the task id.
+            try:
+                result_data = async_result.get()  # get the result from the backend.
+                result = Result(host="self.inventory.hosts[hostname]", result=result_data)
+                multi_result.append(result)
+            except Exception as e:
+                result = Result(
+                    host="self.inventory.hosts[hostname]", result=f"Error retrieving result: {e}", failed=True
+                )
+                multi_result.append(result)
+
+        return AggregatedResult(task.name, kwargs=multi_result)
+
+    # def run(self, task: Task, hosts: List[Host]) -> AggregatedResult:
+    #     print("Running task in Celery")
+    #     # print(f"Task: {task}")
+    #     # print(f"Hosts: {hosts}")
+    #     # print(type(task))
+    #     # print(dir(task))
+    #     result = AggregatedResult(task.name)
+    #     for host in hosts:
+    #         sig = signature(
+    #             "nautobot_plugin_nornir.jobs.run_nornir_packaged_task",
+    #             args=(task.name, dict(host)),
+    #             kwargs={},
+    #             routing_key="default",
+    #             hostname=gethostname(),
+    #         ).delay()
+    #         print(sig)
+    #         result[host.name] = sig.id
+    #     print(result)
+
+    # print(type(result))
+    # print(dir(result))
+    # return dict(result)
+
+    # def run(self, task: Task, hosts: List[Host]) -> AggregatedResult:
+    #     print("Running task in Celery")
+    #     print(f"Task: {task}")
+    #     print(f"Hosts: {hosts}")
+    #     result = AggregatedResult(task.name)
+    #     # group_of_tasks = [signature(task.copy().start(host)) for host in hosts]
+    #     for host in hosts:
+    #         sig = signature(
+    #             "nautobot_plugin_nornir.jobs.run_nornir_packaged_task",
+    #             args=(task, dict(host)),
+    #             kwargs={},
+    #             routing_key="default",
+    #             hostname=gethostname(),
+    #         ).delay()
+    #         print(f"Signature: {sig}")
+    #         result[host.name] = sig.id
+    #     # group_of_tasks = [
+    #     #     signature(
+    #     #         "nautobot_plugin_nornir.jobs.run_nornir_packaged_task",
+    #     #         args=(dict(host),),
+    #     #         kwargs={},
+    #     #         routing_key="default",
+    #     #         hostname=gethostname(),
+    #     #     )
+    #     #     for host in hosts
+    #     # ]
+    #     celery_group = group(group_of_tasks)
+    #     group_tasks = celery_group.apply_async()
+    #     while not group_tasks.ready():
+    #         print("Waiting for results")
+    #         print(group_tasks.ready())
+    #         time.sleep(1)
+    #     print("Group results are ready")
+    #     group_result = group_tasks.results
+    #     # <class 'celery.result.AsyncResult'>
+    #     for index, host in enumerate(hosts):
+    #         mr = task.copy().start(host)
+    #         print(mr)  # <class 'nornir.core.task.MultiResult'>
+    #         result[host.name] = mr.namegroup_result[index].id
+    #     return result
+
+
+# ['TimeoutError', 'class', 'copy', 'del', 'delattr', 'dict', 'dir', 'doc',
+# 'eq', 'format', 'ge', 'getattribute', 'getstate', 'gt', 'hash', 'init',
+# 'init_subclass', 'le', 'lt', 'module', 'ne', 'new', 'reduce', 'reduce_args',
+# 'reduce_ex', 'repr', 'setattr', 'sizeof', 'str', 'subclasshook', 'weakref',
+# '_cache', '_get_task_meta', '_ignored', '_iter_meta', '_maybe_reraise_parent_error',
+# '_maybe_set_cache', '_on_fulfilled', '_parents', '_set_cache', '_to_remote_traceback',
+# 'app', 'args', 'as_list', 'as_tuple', 'backend', 'build_graph', 'children', 'collect',
+# 'date_done', 'failed', 'forget', 'get', 'get_leaf', 'graph', 'id', 'ignored', 'info',
+# 'iterdeps', 'kwargs', 'maybe_reraise', 'maybe_throw', 'name', 'on_ready', 'parent',
+# 'queue', 'ready', 'result', 'retries', 'revoke', 'revoke_by_stamped_headers', 'state',
+#  'status', 'successful', 'supports_native_join', 'task_id', 'then', 'throw', 'traceback',
+# 'wait', 'worker']
