ee881fa8bb7aec7f2aa51fcfbcbeef4083851f38
Gary Snider <75227981+gsnider2195@users.noreply.github.com>
2025-04-18 10:43:00 -0700

    create next-3.0 branch

--- tasks.py
+++ tasks.py
@@ -51,7 +51,7 @@ namespace = Collection("nautobot_plugin_nornir")
 namespace.configure(
     {
         "nautobot_plugin_nornir": {
-            "nautobot_ver": "2.3.1",
+            "nautobot_ver": "next",  # TODO: Change to 3.0.0 after v3.0.0 is released
             "project_name": "nautobot-plugin-nornir",
             "python_ver": "3.11",
             "local": False,
