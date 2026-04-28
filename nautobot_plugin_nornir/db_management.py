64e7125d17ac66013d2507927cab1b30ca601d04
Joe Wesch <joe.wesch@networktocode.com>
2026-04-28 12:43:06 -0500

    Changed close_threaded_db_connections to preserve the wrapped function metadata

--- nautobot_plugin_nornir/db_management.py
+++ nautobot_plugin_nornir/db_management.py
@@ -1,5 +1,7 @@
 """Functions to manage DB related tasks."""
 
+import functools
+
 from django.db import connections
 
 from nautobot_plugin_nornir.constants import NORNIR_SETTINGS
@@ -10,6 +12,7 @@ RUNNER_SETTINGS = NORNIR_SETTINGS.get("runner", {})
 def close_threaded_db_connections(func):
     """Decorator that clears idle DB connections in thread."""
 
+    @functools.wraps(func)
     def inner(*args, **kwargs):
         """Inner function."""
         try:
